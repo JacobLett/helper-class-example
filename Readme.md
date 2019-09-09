87c242918fce5b4a279959dd469a644f27bc9913
Jacob Lett <jlett@altair.com>
2019-09-09 14:35:23 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+﻿# Javascript helper class tutorial
+
+https://www.youtube.com/watch?v=2ZphE5HcQPQ
---
+++ js/helper-class.js
@@ -0,0 +1,42 @@
+﻿/*
+Class -> Object
+(human being)
+
+Instance properties: What they have
+(Data about that object)
+- name
+- height
+- width
+
+Instance Methods - what they do
+(actions)
+- talk
+- run
+ jump
+
+*/
+
+// Class definition
+class MakeChart {
+    // Class constructor, runs once when the opject is created. used to setup an object
+
+    // add arguments to constructor for the properties you need
+    constructor (_width, _height, _color) {
+        console.log('the object is being created');
+        // this refers to the current object
+
+        // instance properties created by the constructor
+        this.width = _width;
+        this.height = _height;
+        this.color = _color;
+
+    }
+
+    // instance methods  actions that can use the data set in the properties
+    getArea () {
+        return this.width = this.height;
+    }
+
+
+
+}
